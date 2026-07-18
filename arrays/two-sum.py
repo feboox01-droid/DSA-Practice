3bdbe50ab2225bbff176efb39da00a588c7d16ea
Abhishek kumar <feboox01@gmail.com>
2026-07-19 02:21:07 +0530

    Update two-sum.py

--- arrays/two-sum.py
+++ arrays/two-sum.py
@@ -1,1 +1,14 @@
+class Solution(object):
 
+    def twoSum(self, nums, target):
+
+        for i in range(len(nums)):
+
+            for j in range(i+1,len(nums)):
+
+                if nums[i]+nums[j]==target:
+
+                    return [i,j] 
+
+
+#this is my first DSA problem i spend a lot of hr to solve this problem i know its just starting face so yaah its interesting but irritating but i continously dicipline my DSA jounery 
